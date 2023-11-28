5e7ddcd9c2fff6a9b2b95cd0d6cb632aba0048ee
Hyunseung Na <57518992+dbfjru@users.noreply.github.com>
2023-11-28 23:50:29 +0900

    Time: 2997 ms (10.68%) | Memory: 17 MB (80.87%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -3,8 +3,10 @@ class Solution:
         output =[]
         for i in range(len(nums)):
             i_val = nums[i]
-            for j in range(i+1,len(nums)):
+            for j in range(1,len(nums)):
                 j_val = nums[j]
+                if i == j:
+                    break
                 if i_val + j_val == target:
                     output.append(i)
                     output.append(j)
